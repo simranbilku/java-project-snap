49c97d7c46626f31637138c3a7cee1b04bf21b57
Simran Bilku <sbilku@hotmail.co.uk>
2026-05-20 16:07:33 +0100

    add enter input handling in playSnap

--- src/main/java/org/cardgame/Snap.java
+++ src/main/java/org/cardgame/Snap.java
@@ -1,8 +1,30 @@
 package org.cardgame;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Snap extends CardGame {
 
     public Snap(String name) {
         super(name);
     }
+
+    public ArrayList<Card> playSnap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press enter to take your turn.");
+        String input = scanner.nextLine();
+
+        if (input.isEmpty()) {
+            dealCard();
+        } else {
+            System.out.println("Try again. Press enter to take your turn.");
+        }
+        return null;
+    }
+
+    // by pressing enter in the command line, the user takes their turn
+    // each turn, a new card is dealt from the deck
+    // the game continues until two cards in a row have the same symbol
+    // at which point the "player" wins and the game ends
+
 }
